36b2e39037ee202f0602f7a539dee096965f3475
atifsalim <atifsalimbhat@gmail.com>
2022-05-29 15:08:03 +0530

    Updates

---
+++ precache-manifest.2e2e9ad3f6e6f25990bd59565693506d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29059f75d65f792f2f4c",
+    "url": "/stopwatch/static/css/main.525a54cc.chunk.css"
+  },
+  {
+    "revision": "29059f75d65f792f2f4c",
+    "url": "/stopwatch/static/js/main.29059f75.chunk.js"
+  },
+  {
+    "revision": "eece99649d387eb7346c",
+    "url": "/stopwatch/static/js/1.eece9964.chunk.js"
+  },
+  {
+    "revision": "650f3d7de6f8ae2fb848",
+    "url": "/stopwatch/static/js/runtime~main.650f3d7d.js"
+  },
+  {
+    "revision": "f6e60137c32be5d867738c444dd8615f",
+    "url": "/stopwatch/index.html"
+  }
+];
